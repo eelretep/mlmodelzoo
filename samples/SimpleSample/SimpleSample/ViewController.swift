3e6b10b43912bc7512b8d512fd19c4c693635659
Peter Lee <eelretep@gmail.com>
2017-07-26 10:44:50 -0700

    support live video for input

--- samples/SimpleSample/SimpleSample/ViewController.swift
+++ samples/SimpleSample/SimpleSample/ViewController.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 import Vision
+import AVFoundation
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+enum ModelInput {
+    case unknown
+    case photoLibrary
+    case camera
+    case video
+}
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var overlayView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var statusLabel: UILabel!
     
@@ -20,6 +30,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var inputImage: UIImage?
     var outputImage: UIImage?
+    
+    let capturePreset = AVCaptureSession.Preset.vga640x480
+    let captureSettings: [String : Any] = [kCVPixelBufferPixelFormatTypeKey as String : kCMPixelFormat_32BGRA]
+    var session: AVCaptureSession!
+    var videoDataOutput: AVCaptureVideoDataOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    let sampleDispatchQueue = DispatchQueue(label: "SampleDispatch")
+    let mlProcessingSlots = DispatchSemaphore(value: 1) // throttle the model processing
+    
+    var modelInput = ModelInput.unknown {
+        didSet {
+            clearOverlay()
+            
+            if modelInput == .video {
+                setupAVSourceCapture()
+                session.startRunning()
+                self.overlayView.backgroundColor = UIColor.clear
+            } else {
+                self.session?.stopRunning()
+                self.overlayView.backgroundColor = UIColor.white
+            }
+        }
+    }
     
     // MARK: - lifecycle
     override func viewDidLoad() {
@@ -33,17 +66,73 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             fatalError("cannot load model:\(error)")
         }
     }
+    
+    // MARK: - video capture
+    func setupAVSourceCapture()
+    {
+        if session?.isRunning == true {
+            return
+        }
+        
+        session = AVCaptureSession()
+        session.sessionPreset = capturePreset
+        
+        let device = AVCaptureDevice.default(for: .video)
+        guard let deviceInput = try? AVCaptureDeviceInput(device: device!) else
+        {
+            fatalError()
+        }
+        
+        if session.canAddInput(deviceInput)
+        {
+            session.addInput(deviceInput)
+        }
+        
+        videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.videoSettings = captureSettings
+        videoDataOutput.alwaysDiscardsLateVideoFrames = true
+        videoDataOutput.setSampleBufferDelegate(self, queue:sampleDispatchQueue)
+        
+        if session.canAddOutput(videoDataOutput!)
+        {
+            session.addOutput(videoDataOutput!)
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewLayer.backgroundColor = UIColor.black.cgColor
+        previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        previewLayer.frame = videoView.bounds
+        
+        previewLayer.connection?.videoOrientation = .portrait
+        videoDataOutput.connection(with: .video)?.videoOrientation = .portrait
+        
+        videoView.layer.addSublayer(previewLayer!)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        // testing
+        //let testImage = UIImage(named: "white.png")!
+        //let pixelBuffer = pixelBufferFromCGImage(image: testImage.cgImage!)!
+        // end testing
+        
+        self.sendRequestToModel(image: nil, orPixelBuffer: pixelBuffer, skipIfBusy: true)
+    }
 
     // MARK: - user interface
     @IBAction func getInputTapped(_ sender: Any) {
-        clearImageView()
-        
         let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+        alert.addAction(UIAlertAction(title: "Take photo", style: .default, handler: { (action) in
+            self.modelInput = .camera
             self.showPicker(source: .camera)
         }))
-        alert.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
+        alert.addAction(UIAlertAction(title: "Pick photo", style: .default, handler: { (action) in
+            self.modelInput = .photoLibrary
             self.showPicker(source: .photoLibrary)
+        }))
+        alert.addAction(UIAlertAction(title: "Live video", style: .default, handler: { (action) in
+            self.modelInput = .video
         }))
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
         }))
@@ -77,12 +166,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         updateLabel(status: nil)
         
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            
             inputImage = image
-            outputImage = nil
             updateImageView(image: image)
             
-            sendRequestToModel(image: image)
+            sampleDispatchQueue.async {
+                self.sendRequestToModel(image: image)
+            }
         }
         
         self.dismiss(animated: true) {
@@ -97,32 +186,53 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     // MARK: - model inference
-    func sendRequestToModel(image: UIImage) {
-        updateLabel(status: "sent request to model")
-
+    func sendRequestToModel(image: UIImage?, orPixelBuffer pixelBuffer:CVPixelBuffer? = nil, skipIfBusy: Bool = false) {
+        assert (image != nil || pixelBuffer != nil)
+        dispatchPrecondition(condition: .onQueue(sampleDispatchQueue))
+        
+        if mlProcessingSlots.wait(timeout:DispatchTime.now()) != .success {
+            if skipIfBusy {
+                return
+            } else {
+                // blocking wait
+                let result = self.mlProcessingSlots.wait(timeout:DispatchTime.distantFuture)
+                assert (result == .success)
+            }
+        }
+        
 #if !USE_COREML_STYLER
-          let requestHandler = VNImageRequestHandler(cgImage: image.cgImage!, orientation: image.cgImagePropertyOrientation(), options:[:])
+    let requestHandler = image != nil ? VNImageRequestHandler(cgImage: image!.cgImage!, orientation: image!.cgImagePropertyOrientation(), options:[:]) :
+        VNImageRequestHandler(cvPixelBuffer: pixelBuffer!, options: [:])
     
-        DispatchQueue.global(qos: .userInitiated).async { // avoid blocking the main thread
+        DispatchQueue.global(qos: .userInitiated).async { // avoid blocking the queue
             do {
                 try requestHandler.perform([self.visionCoreMLRequest])
+                self.mlProcessingSlots.signal()
             } catch {
                 print(error)
                 self.updateLabel(status: error.localizedDescription)
+                self.mlProcessingSlots.signal()
             }
         }
 #else //iOS11 beta3 - CoreML workaround for internal Vision bug when resizing images to fit model input dimensions
         let inputWidth = 480
         let inputHeight = 640
     
-        DispatchQueue.global(qos: .userInitiated).async { // avoid blocking the main thread
+        DispatchQueue.global(qos: .userInitiated).async { // avoid blocking the queue
             do {
-                let inPixelBuffer = CGImageToPixelBufferRGB(image.cgImage!, width: inputWidth, height: inputHeight)!
-                let outPixelBuffer = try self.modelzoo.prediction(input: mlmodelzooInput(input1: inPixelBuffer)).output1
-                self.updateOutputWithPixelBuffer(outPixelBuffer, width: Int(image.size.width), height: Int(image.size.height), orientation: image.imageOrientation)
+                if let image = image {
+                    let inPixelBuffer = CGImageToPixelBufferRGB(image.cgImage!, width: inputWidth, height: inputHeight)!
+                    let outPixelBuffer = try self.modelzoo.prediction(input: mlmodelzooInput(input1: inPixelBuffer)).output1
+                    self.updateOutputWithPixelBuffer(outPixelBuffer, width: Int(image.size.width), height: Int(image.size.height), orientation: image.imageOrientation)
+                } else {
+                    let outPixelBuffer = try self.modelzoo.prediction(input: mlmodelzooInput(input1: pixelBuffer!)).output1
+                    self.updateOutputWithPixelBuffer(outPixelBuffer, width: inputWidth, height: inputHeight, orientation: UIImageOrientation.up)
+                }
+                self.mlProcessingSlots.signal()
             } catch {
                 print(error)
                 self.updateLabel(status: error.localizedDescription)
+                self.mlProcessingSlots.signal()
             }
         }
 #endif
@@ -134,7 +244,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
         } else {
             print("unexpected result type")
-            return
         }
     }
     
@@ -159,12 +268,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    func clearImageView() {
+    func clearOverlay() {
+        inputImage = nil
+        outputImage = nil
         imageView.image = nil
         statusLabel.text = nil
     }
     
-    func updateImageView(image: UIImage) {
+    func updateImageView(image: UIImage?) {
         DispatchQueue.main.async {
             UIView.transition(with: self.imageView, duration: 0.3, options: .transitionCrossDissolve, animations: {
                 self.imageView.image = image
